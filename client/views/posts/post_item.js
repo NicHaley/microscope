1fbdea637528fd05fdef8fde64b9510f3ae5a53a
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-04 11:36:44 -0500

    Added a posts collection

--- client/views/posts/post_item.js
+++ client/views/posts/post_item.js
@@ -1,7 +1,8 @@
 Template.postItem.helpers({
 	domain: function(){
 		var a = document.createElement('a');
+		// This refers to the iterated object specifed in the posts_list.html #each iterator
 		a.href = this.url;
 		return a.hostname;
 	}
-})
+});
