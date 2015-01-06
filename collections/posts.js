8f712eab4b16fbf1c8b049f9d71feaf460ca2e3f
Nicholas Haley <nicholaswilliamhaley@gmail.com>
2015-01-06 11:54:49 -0500

    Sort posts by submitted timestamp

--- collections/posts.js
+++ collections/posts.js
@@ -3,6 +3,7 @@ Posts = new Meteor.Collection('posts');
 Meteor.methods({
 	postInsert: function(postAttributes) {
 		var user = Meteor.user(),
+		// First check to see if a post with the same link already exists
 		postWithSameLink = Posts.findOne({url: postAttributes.url});
 		// ensure the user is logged in
 		if (!user)
@@ -10,17 +11,21 @@ Meteor.methods({
 		// ensure the post has a title
 		if (!postAttributes.title)
 			throw new Meteor.Error(422, 'Please fill in a headline');
-		// check that there are no previous posts with the same link
+		// throw an error for posts with the same link
 		if (postAttributes.url && postWithSameLink) { throw new Meteor.Error(302,
 			'This link has already been posted',
 			postWithSameLink._id); }
-		// pick out the whitelisted keys
-		var post = _.extend(_.pick(postAttributes, 'url', 'title', 'message'), { userId: user._id,
+		// pick out the fields that we want to insert
+		var post = _.extend(_.pick(postAttributes, 'url', 'title'), 
+			{ userId: user._id,
 			author: user.username,
 			submitted: new Date().getTime()
 		});
+
+		//Insert the post
 		var postId = Posts.insert(post);
-		
+
+		//Return the new post's id to the user
 		return postId; 
 	}
 });
